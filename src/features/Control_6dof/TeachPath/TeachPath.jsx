2482afe81145832954709520d182a5b8d035777f
IamPhuong2809 <nminhphuong2809@gmail.com>
2025-03-01 01:46:46 +0700

    first

--- src/features/Control_6dof/TeachPath/TeachPath.jsx
+++ src/features/Control_6dof/TeachPath/TeachPath.jsx
@@ -63,25 +63,17 @@ const TeachPath = () => {
 
     const handlePathClose = () => {
         setIsPointClosing(true);
+        setIsDetailClosing(true);
         setTimeout(() => {
             setIsPointOpen(false);
             setSelectedPath(null);
             setIsPointClosing(false);
+
+            setIsDetailOpen(false);
+            setSelectedPoint(null);
+            setIsDetailClosing(false);
         }, 500);
     };
-
-    const PointList = () => {
-        return (
-            <List 
-                items={points} 
-                SelectedItem={selectedPoint}
-                handleItemSelect={handlePointSelect} 
-                handleDetailClose={handleDetailClose}
-                isPopupClosing={isPointClosing} 
-                headerName="Point Name" 
-            />
-        )
-    }
     //#endregion
 
     //#region Detail Point
@@ -97,7 +89,7 @@ const TeachPath = () => {
                 setIsDetailOpen(true);
             }, 500);
         }
-        else {
+        else {  
             setSelectedPoint(point);
             setIsDetailOpen(true);
         }
@@ -112,11 +104,122 @@ const TeachPath = () => {
         }, 500);
     };
 
+    const coordinateRows = [
+        [
+          { label: 'X', main: '+0.00', sub: '+199.99' },
+          { label: 'Y', main: '-210.00', sub: '-210.00' },
+          { label: 'Z', main: '+495.00', sub: '+399.97' }
+
+        ],
+        [
+          { label: 'RX', main: '+180.00', sub: '-179.99' },
+          { label: 'RY', main: '+0.00', sub: '-0.01' },
+          { label: 'RZ', main: '+81.99', sub: '+81.99' }
+        ],
+        [
+            { label: 'Tool', main: '0', sub: '0' },
+            { label: 'Figure', main: '+5', sub: '+5' },
+            { label: 'Work', main: '0', sub: '0' }
+        ]
+      ];
+
+    // Khai báo mảng cấu hình các ô cần hiển thị
+    const parameterConfig = [
+        {
+        label: 'Motion Typ',
+        type: 'select',
+        options: ['LIN', 'PTP', 'CIRC'],   // 3 item cho dropdown
+        defaultValue: 'LIN'
+        },
+        {
+        label: 'CONT',
+        type: 'select',
+        options: ['FALSE', 'TRUE', 'MIX'], // 3 item cho dropdown
+        defaultValue: 'FALSE'
+        },
+        {
+        label: 'Stop Point',
+        type: 'select',
+        options: ['NO', 'YES', 'MAYBE'],   // 3 item cho dropdown
+        defaultValue: 'NO'
+        },
+        {
+        label: 'M_Function',
+        type: 'text',
+        defaultValue: '999'
+        },
+        {
+        label: 'Velocity',
+        type: 'text',
+        defaultValue: '10 %'
+        },
+        {
+        label: 'Acceleration',
+        type: 'text',
+        defaultValue: '10 %'
+        },
+        {
+        label: 'Corner distanc',
+        type: 'text',
+        defaultValue: '100mm'
+        }
+    ];
+
     const PopupScreen = () => {
         return(
-            <div>abc</div>
+            <div className={`point-detail ${isDetailClosing ? 'slide-out' : 'slide-in'}`}>
+                <div className="point-detail-header">
+                    <div className="point-detail-title">[Point {selectedPoint.id}] "{selectedPoint.name}"</div>
+                    <button className="close-button" onClick={handleDetailClose}>✕</button>
+                </div>
+                <div className="coordinates-container">
+                    {coordinateRows.map((row, rowIndex) => (
+                        <div className="coordinates-row" key={rowIndex}>
+                            {row.map((cell, cellIndex) => (
+                                <div className="coordinates-cell" key={cellIndex}>
+                                    <div className="coordinate-label">{cell.label}</div>
+                                    <input className="coordinate-value-input" type="text" value={cell.main} />
+                                    <div className="coordinate-value-main">{cell.sub}</div>
+                                </div>
+                            ))}
+                        </div>
+                    ))}
+                </div>
+
+                <div className="parameter-form-container">
+                    {parameterConfig.map((param, index) => (
+                        <div className="parameter-item" key={index}>
+                            <label className="parameter-label">{param.label}</label>
+                            {param.type === 'select' ? (
+                                <select
+                                className="parameter-select"
+                                defaultValue={param.defaultValue}
+                                >
+                                    {param.options.map((option, idx) => (
+                                        <option key={idx} value={option}>
+                                        {option}
+                                        </option>
+                                    ))}
+                                </select>
+                            ) : (
+                                <input
+                                className="parameter-input"
+                                type="text"
+                                defaultValue={param.defaultValue}
+                                />
+                            )}
+                        </div>
+                    ))}
+                </div>
+
+                <div className='button-container'>
+                    <button className='button-item'>Jog</button>
+                    <button className='button-item'>Teach</button>
+                    <button className='button-save'>Save</button>
+                </div>
+            </div>
         )
-    }
+    };
     //#endregion
     
   return (
@@ -132,8 +235,22 @@ const TeachPath = () => {
                 handleDetailClose={handlePathClose}
                 headerName="Path Name"
             />
-            {/* Hiển thị PopupScreen nếu có item được chọn */}
-            {isPointOpen && PointList && <PointList />}
+            {/* Point List (danh sách thứ hai) */}
+            {isPointOpen && (
+                <List 
+                items={points} 
+                SelectedItem={selectedPoint}
+                handleItemSelect={handlePointSelect} 
+                handleDetailClose={handleDetailClose}
+                isPopupClosing={isPointClosing} 
+                headerName="Point Name" 
+                />
+            )}
+            
+            {/* Detail Screen cho Point được chọn */}
+            {isDetailOpen && selectedPoint && (
+                <PopupScreen />
+            )}
         </div>
    </div>
   )
